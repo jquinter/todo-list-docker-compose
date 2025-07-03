e75739e2a7861ac254b833302cb1e3d4a21c1acc
Profe Julio <jquinter@gmail.com>
2025-07-03 17:25:19 -0400

    Permission fix for datastore folder requires sudo

--- gcp_cloudshell_dockercompose_upgrade.sh
+++ gcp_cloudshell_dockercompose_upgrade.sh
@@ -3,6 +3,6 @@ sudo curl -L "https://github.com/docker/compose/releases/download/${TV}/docker-c
 sudo chmod +x /usr/local/bin/docker-compose
 echo "Docker Compose has been upgraded to ${TV}"
 
-chmod -R 777 datastore
+sudo chmod -R 777 datastore
 
 cp .env.template .env
