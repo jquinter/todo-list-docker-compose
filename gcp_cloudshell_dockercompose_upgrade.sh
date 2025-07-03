336da51976bf1128ff174c2a2ddddc37dcc5b1f6
Profe Julio <jquinter@gmail.com>
2025-07-03 14:17:37 -0400

    Required fix for datastore mounting

--- gcp_cloudshell_dockercompose_upgrade.sh
+++ gcp_cloudshell_dockercompose_upgrade.sh
@@ -2,3 +2,5 @@ TV="v2.38.1"
 sudo curl -L "https://github.com/docker/compose/releases/download/${TV}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 echo "Docker Compose has been upgraded to ${TV}"
+
+chmod -R 777 datastore
