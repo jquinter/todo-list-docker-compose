7ac1376d950dd7e203ef100e864b73ccffd2c7c1
Profe Julio <jquinter@gmail.com>
2025-07-03 08:22:25 -0400

    Patching docker compose in GCP cloudshell

---
+++ gcp_cloudshell_dockercompose_upgrade.sh
@@ -0,0 +1,4 @@
+TV="v2.38.1"
+sudo curl -L "https://github.com/docker/compose/releases/download/${TV}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+echo "Docker Compose has been upgraded to ${TV}"
